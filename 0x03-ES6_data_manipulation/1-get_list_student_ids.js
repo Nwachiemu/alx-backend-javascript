32fc7fa317d53d6b5c088f88acc6b0eae844b12d
Nwachiemu <emuchaynwachi@gmail.com>
2024-06-07 00:13:31 +0100

    added

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,15 +1,10 @@
-/**
- * Retrieves ids from a list of students.
- * @param {{
-*   id: Number,
-*   firstName: String,
-*   location: String
-* }[]} students - The list of students.
-* @returns
-*/
-export default function getListStudentIds(students) {
- if (students instanceof Array) {
-   return students.map((student) => student.id);
- }
- return [];
-}
+const getListStudentIds = (listStudents) => {
+  let listStudentIds = [];
+  if (!(listStudents instanceof Array)) {
+    return listStudentIds;
+  }
+  listStudentIds = listStudents.map((student) => student.id);
+  return listStudentIds;
+};
+
+export default getListStudentIds;
